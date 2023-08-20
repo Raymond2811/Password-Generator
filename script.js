749e34932bafc4720e1ce4961b15e90f588d6223
Raymond Bautista <ray@Raymonds-MacBook-Pro.local>
2023-08-19 17:17:16 -0700

    added function to generate password

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+function generatePassword(){
+  var charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+';
+  var passwordLength = 8;
+  var password = '';
+}
+
+for (var i = 0; i <passwordLength; i++){
+  var random = 
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
